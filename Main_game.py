650ca0389239d0e8191961793c740457e004e2ed
rasser31 <rasser31@hotmail.com>
2020-04-01 08:44:04 +0200

    a

--- Main_game.py
+++ Main_game.py
@@ -1,12 +1,14 @@
 import math
 import pygame
 
-
-
-
 pygame.init()
 
+display_x = 1000
+display_y = 600
 
-gameDisplay = pygame.display.set_mode((1000,600))
+gameDisplay = pygame.display.set_mode((display_x,display_y))
 
-
+def message_display(text):
+    hitText = pygame.font.Font('freesansbold.ttf', 75)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_x/2),(display_y))
