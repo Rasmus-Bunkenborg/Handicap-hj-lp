ec2c0d9bb5c5130892f3ac5dc516cc91a7eaa426
PreussenBlue <bibbunk@gmail.com>
2020-04-01 09:27:04 +0200

    Update Main_game.py

--- Main_game.py
+++ Main_game.py
@@ -1,12 +1,17 @@
 import math
 import pygame
 
-
-
-
 pygame.init()
 
-
 gameDisplay = pygame.display.set_mode((1000,600))
+penguinImage = pygame.image.load()
 
 
+finished = False
+while not finished:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    
+    pygame.display.flip()
+pygame.Quit()
