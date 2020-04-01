f927b6b76deca788e8848146df05f1e8eb40640e
rasser31 <rasser31@hotmail.com>
2020-04-01 09:28:36 +0200

    a

--- Main_game.py
+++ Main_game.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+import random
 
 
 
@@ -10,3 +11,26 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((1000,600))
 
 
+<<<<<<< Updated upstream
+=======
+def hitMissText():
+    font = pygame.font.SysFont("comicsansms", 72)
+    textHit = font.render("You hit!", False, (84, 194, 45))
+    textMiss = font.render("You missed!", False, (163, 41, 33))
+    
+def asdasd():
+    if textHit == True:
+        screen.blit(textHit, (display_x/2, display_y/2))
+    if textMiss == True:
+        screen.blit(textMiss, (display_x/2, display_y/2))
+        
+rand = (random.randrange(1, 3))
+if rand == 1:
+    textHit = True
+    textMiss = False
+    asdasd()
+if rand == 2:
+    textHit = False
+    textMiss = True
+    asdasd()
+>>>>>>> Stashed changes
