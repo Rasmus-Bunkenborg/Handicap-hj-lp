af1a7a66f36e66c213ed04f6e50ed1e2064d1d59
xandermand <54107781+xandermand@users.noreply.github.com>
2020-04-21 09:40:39 +0200

    added sprites n stuff

--- Main_game.py
+++ Main_game.py
@@ -3,10 +3,29 @@ import pygame
 
 
 
+veg = pygame.image.load("Vegeta.png")
+goku = pygame.image.load("Goku.png")
+back = pygame.image.load("Possible_background.jpg")
+def main():
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((1000,600))
 
-pygame.init()
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:
+            break
+
+        gameDisplay.fill((255, 255, 255))
+
+        gameDisplay.blit(back, (0, 0))
+
+        gameDisplay.blit(goku, (150, 480))
+
+        gameDisplay.blit(veg, (750, 480))
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 
-gameDisplay = pygame.display.set_mode((1000,600))
-
-
+main()
