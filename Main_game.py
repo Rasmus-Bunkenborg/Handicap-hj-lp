ce81ede9c2be122730145225aceb966a37623752
rasser31 <rasser31@hotmail.com>
2020-04-29 10:19:35 +0200

    Yes

--- Main_game.py
+++ Main_game.py
@@ -3,7 +3,18 @@ import pygame
 import random
 
 
+<<<<<<< Updated upstream
 
+=======
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
+veg = pygame.image.load("Vegeta.png")
+goku = pygame.image.load("Goku.png")
+back = pygame.image.load("Possible_background.jpg")
+def main():
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((1000,600))
+>>>>>>> Stashed changes
 
 pygame.init()
 
@@ -11,6 +22,7 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((1000,600))
 
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 =======
 def hitMissText():
@@ -33,4 +45,66 @@ if rand == 2:
     textHit = False
     textMiss = True
     asdasd()
+>>>>>>> Stashed changes
+=======
+        gameDisplay.blit(veg, (750, 480))
+
+        pygame.display.flip()
+
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        gameDisplay.fill((30, 30, 30))
+        # Render the current text.
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        gameDisplay.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pygame.draw.rect(gameDisplay, color, input_box, 2)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
+    pygame.quit()
+
+
+    pygame.quit()
+
+
+main()
 >>>>>>> Stashed changes
