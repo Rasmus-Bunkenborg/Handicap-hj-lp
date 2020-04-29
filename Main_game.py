a8f2b239ed7895586c0f0730083e528a0706cbb5
xandermand <54107781+xandermand@users.noreply.github.com>
2020-04-29 10:22:28 +0200

    Update Main_game.py

--- Main_game.py
+++ Main_game.py
@@ -6,6 +6,12 @@ import pygame
 veg = pygame.image.load("Vegeta.png")
 goku = pygame.image.load("Goku.png")
 back = pygame.image.load("Possible_background.jpg")
+
+gbeam = pygame.image.load("Blast.png")
+vbeam = pygame.image.load("Blast_reverse.png")
+
+
+
 def main():
     pygame.init()
     gameDisplay = pygame.display.set_mode((1000,600))
@@ -14,14 +20,19 @@ def main():
         ev = pygame.event.poll()
         if ev.type == pygame.QUIT:
             break
+        def gshot():
+            gameDisplay.blit(gbeam, (200, 500))
 
+        def vshot():
+            gameDisplay.blit(vbeam, (200, 485))
+            
         gameDisplay.fill((255, 255, 255))
 
         gameDisplay.blit(back, (0, 0))
 
         gameDisplay.blit(goku, (150, 480))
 
-        gameDisplay.blit(veg, (750, 480))
+        gameDisplay.blit(veg, (680, 480))
 
         pygame.display.flip()
 
