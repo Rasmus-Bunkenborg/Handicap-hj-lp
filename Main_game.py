b5d817ceab587b0054ede40d8419fcefbdbf8e53
rasser31 <rasser31@hotmail.com>
2020-05-06 20:57:35 +0200

    asd

--- Main_game.py
+++ Main_game.py
@@ -1,73 +1,43 @@
-import pygame as pg
+import math
+import pygame
+
+
+
+veg = pygame.image.load("Vegeta.png")
+goku = pygame.image.load("Goku.png")
+back = pygame.image.load("Possible_background.jpg")
+
+gbeam = pygame.image.load("Blast.png")
+vbeam = pygame.image.load("Blast_reverse.png")
+
+
 
 def main():
-    veg = pg.image.load("Vegeta.png")
-    goku = pg.image.load("Goku.png")
-    back = pg.image.load("Possible_background.jpg")
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((1000,600))
 
-    gbeam = pg.image.load("Blast.png")
-    vbeam = pg.image.load("Blast_reverse.png")
-    screen = pg.display.set_mode((1000, 600))
-    font = pg.font.Font(None, 32)
-    clock = pg.time.Clock()
-    input_box = pg.Rect(400, 200, 140, 32)
-    color_inactive = pg.Color('red')
-    color_active = pg.Color('pink')
-    color = color_inactive
-    active = False
-    text = ''
-    done = False
-            
-    while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-            if event.type == pg.MOUSEBUTTONDOWN:
-                # If the user clicked on the input_box rect.
-                if input_box.collidepoint(event.pos):
-                    # Toggle the active variable.
-                    active = not active
-                else:
-                    active = False
-                # Change the current color of the input box.
-                color = color_active if active else color_inactive
-            if event.type == pg.KEYDOWN:
-                if active:
-                    if event.key == pg.K_RETURN:
-                        print(text)
-                        text = ''
-                    elif event.key == pg.K_BACKSPACE:
-                        text = text[:-1]
-                    else:
-                        text += event.unicode
-
-        screen.fill((30, 30, 30))
-        # Render the current text.
-        txt_surface = font.render(text, True, color)
-        # Resize the box if the text is too long.
-        width = max(200, txt_surface.get_width()+10)
-        input_box.w = width
-        screen.blit(back, (0,0))
-        # Blit the right character
-        screen.blit(goku, (150, 480))
-        # Blit the left character
-        screen.blit(veg, (680, 480))
-        
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:
+            break
         def gshot():
-            screen.blit(gbeam, (200, 500))
+            gameDisplay.blit(gbeam, (200, 500))
 
         def vshot():
-            screen.blit(vbeam, (200, 485))
-        # Blit the text.
-        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        # Blit the input_box rect.
-        pg.draw.rect(screen, color, input_box, 2)
-        
-        pg.display.flip()
-        clock.tick(30)
-        
-if __name__ == '__main__':
-    pg.init()
-    main()
-    pg.quit()
+            gameDisplay.blit(vbeam, (200, 485))
+            
+        gameDisplay.fill((255, 255, 255))
+
+        gameDisplay.blit(back, (0, 0))
+
+        gameDisplay.blit(goku, (150, 480))
+
+        gameDisplay.blit(veg, (680, 480))
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+main()
 
